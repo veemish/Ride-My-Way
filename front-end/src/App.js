104c7843527b3fdbf8244e79de487a0d284f4aad
veemish <vlikunama@gmail.com>
2019-05-26 00:09:10 +0300

    progress on cresting fornt end using react app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Component/index";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PassangerRegister from "./Component/Authentification/PassangerRegister";
+import DriverRegister from "./Component/Authentification/DriverRegister";
+import Passangerlogin from "./Component/Authentificatiion/Passangerlogin";
+import Driverlogin from "./Component/Authetification/Driverlogin";
+import DriverProfile from "./Component/Authentification/DriverProfile";
+import PassangerProfile from "./Component/Authentification/PassangerProfile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/passanger_register" component={PassangerRegister} />
+        <Route path="/driver_register" component={DriverRegister} />
+        <Route path="/passanger_login" component={Passangerlogin} />
+        <Route path="/driver_login" component={Driverlogin} />
+        <Route path="/driver_profile" component={DriverProfile} />
+        <Route path="/passanger_profile" component={PassangerProfile} />
+      </div>
+    </Router>
   );
 }
 
